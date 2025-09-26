f127f133670bc0d2ba34102e0bfdfe2ac4e66b86
juanlu2007 <juanlu.pb.07@gmail.com>
2025-09-26 17:18:11 +0200

    Add files via upload

--- SRC/viaje_ej2.py
+++ SRC/viaje_ej2.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
+
 distancia_km =int(input("Introduzca la distancia en km")) 
 velocidad_kmh =int(input("Introduzca la velocidad en km/h")) 
 tiempo_horas = distancia_km // velocidad_kmh
 tiempo_dias = tiempo_horas // 24
-=======
-distancia_km =int(input("Introduzca la distancia en km")) 
-velocidad_kmh =int(input("Introduzca la velocidad en km/h")) 
-tiempo_horas = distancia_km // velocidad_kmh
-tiempo_dias = tiempo_horas // 24
->>>>>>> f775e53483e35acf5471524dfc1e6e844d59efa9
 print(f"Tardarías {tiempo_dias} días en llegar.")
--- SRC/viaje_ej3.py
+++ SRC/viaje_ej3.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 edad=int(input("Introduza su edad"))
 lv_fisico=int(input("introduzca su nivel fisico (1-10)"))
 while lv_fisico>10 or lv_fisico<1:
@@ -8,15 +8,4 @@ if edad<18:
 elif lv_fisico<5:
      print("Debe mejorar su forma fisica")
 else:
-=======
-edad=int(input("Introduza su edad"))
-lv_fisico=int(input("introduzca su nivel fisico (1-10)"))
-while lv_fisico>10 or lv_fisico<1:
-    lv_fisico=int(input("introduzca su nivel fisico (1-10)"))
-if edad<18:
-    print("Debe ser mayor de edad para embarcarse")
-elif lv_fisico<5:
-     print("Debe mejorar su forma fisica")
-else:
->>>>>>> f775e53483e35acf5471524dfc1e6e844d59efa9
     print("¡Listo para despegar!")
